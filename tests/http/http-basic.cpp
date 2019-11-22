9a92b0dcf0ec5d5b8fc10c6991cd8f4297e0d7ae
Sylvain Garcia <garcia.6l20@gmail.com>
2019-11-22 12:09:05 +0100

    custom body type example

--- tests/http/http-basic.cpp
+++ tests/http/http-basic.cpp
@@ -170,3 +170,139 @@ TEST(HttpBasicTest, FileUpload) {
 
     srvctx.stop();
 }
+
+template <size_t sz>
+struct ro_array_body {
+    using value_type = std::array<char, sz>;
+
+    static
+    std::uint64_t
+    size(value_type const& body)
+    {
+        return body.size();
+    }
+
+    class reader {
+
+        value_type& _body;
+        size_t _offset = 0;
+
+    public:
+        template<bool isRequest, class Fields>
+        explicit
+        reader(net::http::header<isRequest, Fields>&, value_type& b)
+            : _body(b)
+        {
+        }
+
+        void
+        init(boost::optional<
+            std::uint64_t> const& length, net::error_code& ec)
+        {
+            if(length)
+            {
+                if(*length > sz)
+                {
+                    ec = net::http::error::buffer_overflow;
+                    return;
+                }
+            }
+            ec = {};
+        }
+
+        template<class ConstBufferSequence>
+        std::size_t
+        put(ConstBufferSequence const& buffers,
+            net::error_code& ec)
+        {
+            auto const extra = net::beast::buffer_bytes(buffers);
+            if (extra > sz - _offset)
+            {
+                ec = net::http::error::buffer_overflow;
+                return 0;
+            }
+
+            ec = {};
+
+            char* dest = &_body[_offset];
+            for(auto b : net::beast::buffers_range_ref(buffers))
+            {
+                std::memcpy(dest, b.data(), b.size());
+                dest += b.size();
+                _offset += b.size();
+            }
+
+            return extra;
+        }
+
+        void
+        finish(net::error_code& ec)
+        {
+            ec = {};
+        }
+    };
+
+    class writer
+    {
+    public:
+        using const_buffers_type =
+            net::beast::net::const_buffer;
+
+        template<bool isRequest, class Fields>
+        explicit
+        writer(net::http::header<isRequest, Fields> const&, value_type const& b)
+        {
+            throw std::runtime_error("read only");
+        }
+
+        void
+        init(net::error_code& ec)
+        {
+            throw std::runtime_error("read only");
+        }
+
+        boost::optional<std::pair<const_buffers_type, bool>>
+        get(net::error_code& ec)
+        {
+            throw std::runtime_error("read only");
+        }
+    };
+};
+
+TEST(HttpBasicTest, CustomBody) {
+    boost::asio::io_context srvctx;
+    using server_type = net::http::server<>;
+    server_type srv{srvctx, {net::ip::address_v4::loopback(), 4242}};
+    srv.add_route("/test", [](server_type::context_type& context) -> server_type::route_return_type {
+        auto request = context.req();
+        net::http::response<net::http::string_body> res{
+            std::piecewise_construct,
+            std::make_tuple("ok"),
+            std::make_tuple(net::http::status::ok, request.version())
+        };
+        res.set(net::http::field::content_type, "text/plain");
+        res.content_length(res.body().size());
+        return std::move(res);
+    });
+
+    auto fut = std::async([&srvctx] {
+        srvctx.run();
+    });
+
+    std::this_thread::sleep_for(500ms);
+
+    boost::asio::io_context ctx;
+    net::error_code ec;
+    net::http::response<ro_array_body<2>> reply;
+    net::http::client::async_get({"http://localhost:4242/test"}, reply, ctx, ec);
+
+    ctx.run();
+
+    auto array = std::move(reply.body());
+
+    ASSERT_EQ('o', array[0]);
+    ASSERT_EQ('k', array[1]);
+
+    srvctx.stop();
+}
+
