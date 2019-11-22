1658bf67c98161339462549ca21cf02b8a86339d
Sylvain Garcia <garcia.6l20@gmail.com>
2019-11-22 08:13:33 +0100

    tmp: working on boost build

--- CMake/GetBoost.cmake
+++ CMake/GetBoost.cmake
@@ -2,18 +2,31 @@ include(ExternalProject)
 
 set(BOOST_VERSION 1.71.0)
 
+set(B2_OPTIONS
+    --with-system
+    --with-coroutine
+)
+
 if (WIN32)
     set(BOOST_CONFIGURE_COMMAND bootstrap.bat)
-    set(BOOST_BUILD_COMMAND b2)
+    set(BOOST_BUILD_COMMAND b2 ${B2_OPTIONS})
+    # as I use to say: windows suck...
+    set(BOOST_EXTRA_SUBMODULES
+        libs/predef
+        libs/winapi
+        libs/date_time
+        libs/throw_exception
+    )
 else()
     set(BOOST_CONFIGURE_COMMAND ./bootstrap.sh)
-    set(BOOST_BUILD_COMMAND ./b2)
+    set(BOOST_BUILD_COMMAND ./b2 ${B2_OPTIONS})
 endif()
 
 ExternalProject_Add(boost
     GIT_REPOSITORY https://github.com/boostorg/boost.git
     GIT_TAG boost-${BOOST_VERSION}
     GIT_SUBMODULES
+        libs/assert
         libs/config
         libs/headers
         libs/atomic
@@ -26,6 +39,7 @@ ExternalProject_Add(boost
         libs/beast
         tools/build
         tools/boost_install
+        ${BOOST_EXTRA_SUBMODULES}
     GIT_SHALLOW YES
     CONFIGURE_COMMAND "${BOOST_CONFIGURE_COMMAND}"
     BUILD_COMMAND "${BOOST_BUILD_COMMAND}"
